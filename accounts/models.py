ea9a497ddac32bd323ae30386d77b3ed0d9abb7d
krozv <krozv.code@gmail.com>
2024-07-05 07:13:39 +0900

    Feat: models.py 수정

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # default user model
-# username(id), password1, password2, email
+# username(id), password1, password2, email, 
 
 class User(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
@@ -10,6 +10,9 @@ class User(AbstractUser):
     nickname = models.CharField(max_length=10, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True, verbose_name="생년월일")
 
+    # 추후 추가되는 field: point, nickname, 배송지입력등,,
+    point = models.IntegerField(default=0, )
+    
     # 디버깅용
     def __str__(self):
         return self.username
