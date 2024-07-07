da85bc1070880e5826869f2776fdbaf9d11a815e
CSchoice <tkdlqjf@gmail.com>
2024-07-08 00:16:43 +0900

    Fix : URL 수정

--- board/urls.py
+++ board/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('posts/', views.post_list, name='post_list'),
     path('posts/<int:pk>/', views.post_detail, name='post_detail'),
     path('posts/search/', views.search_posts, name='search_posts'),
-    path('posts/<int:post_id>/comments/', views.comment_list, name='comment-list'),
+    path('posts/<int:post_id>/comments/', views.comment_list, name='comment_list'),
+    path('posts/<int:post_id>/comments/<int:comment_id>/', views.comment_operations, name='comment_operations'),
+    path('posts/<int:post_id>/comments/<int:comment_id>/detail/', views.comment_detail, name='comment_detail'),
 ]
