fd1d037c8287d136f210d11f8708fc3f2f0c1cda
krozv <krozv.code@gmail.com>
2024-08-01 04:19:00 +0900

    Feat : 카카오 로그인 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,10 @@ from . import views
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),  # 로그인, 로그아웃, 비밀번호 변경 등의 엔드포인트
     path('registration/', include('dj_rest_auth.registration.urls')),  # 회원가입 엔드포인트
-    path('social/', include('allauth.urls')), # 소셜로그인 엔드포인트
     # google login endpoint
     path('google/login/', views.google_login, name="google_login"),
     path('google/callback/', views.google_callback, name="google_callback"),
+    path('kakao/login/', views.kakao_login, name="kakao_login"),
+    path('kakao/callback/', views.kakao_callback, name="kakao_callback"),
+    
 ]
