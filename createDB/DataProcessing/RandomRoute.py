75eff2f4bc79303419d6d70a00bbba3f05123ebb
address0 <soyong6624@gmail.com>
2024-09-13 21:33:23 +0900

    [DB] feat: 랜덤 여행지 추천 갯수 변경

--- createDB/DataProcessing/RandomRoute.py
+++ createDB/DataProcessing/RandomRoute.py
@@ -6,7 +6,7 @@ from .FilterName import filter_type
 
 def random_area(areacode):
     tour_data = Tours.objects.filter(sigungucode=areacode)
-    random_tour_data = random.sample(list(tour_data), min(len(tour_data), 5))
+    random_tour_data = random.sample(list(tour_data), min(len(tour_data), 10))
     random_data_json = json.loads(serializers.serialize('json', random_tour_data))
     return random_data_json
 
@@ -14,7 +14,7 @@ def random_area(areacode):
 def random_tour_type(areacode, tour_type):
     tour_data = filter_type(areacode, tour_type)
     if tour_data:
-        random_tour_data = random.sample(list(tour_data), min(len(tour_data), 5))
+        random_tour_data = random.sample(list(tour_data), min(len(tour_data), 10))
         random_data_json = json.loads(serializers.serialize('json', random_tour_data))
     else:
         random_data_json = '여행 유형 입력이 올바르지 않습니다.'
